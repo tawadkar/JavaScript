1e8afd105c01be305795376b605c70cf774e9419
Tanmay <tanmaywadkar7@gmail.com>
2021-10-01 12:34:36 +0530

    S4 DataTypes

---
+++ js/session4.js
@@ -0,0 +1,44 @@
+//String >>String
+let name = "Tanmay"
+console.log("My string is" + name);
+console.log("Data type is "+ (typeof name));
+
+//Number >>number
+let marks = 34;
+console.log("Data type is "+ (typeof marks));
+
+
+//Boolean >>boolean
+let isDriver = true;
+console.log("Data type is "+ (typeof isDriver));
+
+//Null>> object
+let nullval = null;
+console.log("Data type is "+ (typeof nullval));
+
+//Undefined  >> undefined
+let undef = undefined;
+console.log("Data type is "+ (typeof undef));
+
+//Reference Data Types
+//Arrays
+let myarr = [1,2,3,4,5,false,"string"]
+console.log("Data type is "+ (typeof myarr));
+
+//Object Literals
+let stMarks = {
+   harry:45,
+   Ashlin:67,
+   Mayank:69
+}
+console.log("Data type is "+ (typeof stMarks));
+
+function findName(){
+
+}
+console.log("Data type is "+ (typeof findName));
+
+
+let date = new Date();
+console.log(typeof date);
+
