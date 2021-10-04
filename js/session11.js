c20eff4f45a73b1a033cc1886438eebaedc1a9d4
Tanmay <tanmaywadkar7@gmail.com>
2021-10-04 21:01:06 +0530

    S11 Manipulaing websites using JS Window Object

---
+++ js/session11.js
@@ -0,0 +1,16 @@
+let a  = window;
+//alert('hello js');
+
+//a= prompt('Type your name') // Allows user to enter value in alert
+//a = confirm ('are you sure?') // ok returns true cancel returns false
+
+a = window.innerHeight;
+a=innerWidth;
+a=scrollX; //returns value after horizontal scroll 
+a=scrollY;
+a=location;
+a=location.toString;
+a = window.history;
+//on console>> location.href = '//facebook.com'
+console.log(a)
+
