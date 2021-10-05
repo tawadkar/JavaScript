bbdc7db44b5b20b6a8b7b2e327002be7fb1cef35
Tanmay <tanmaywadkar7@gmail.com>
2021-10-05 11:45:04 +0530

    S12 Understanding DOM and creating website Layout

---
+++ js/session12.js
@@ -0,0 +1,11 @@
+let a = document;
+a=document.all
+//a=document.body
+//a=document.forms[0]
+
+// Array.from(a).forEach(function(element){
+//     console.log(element)
+
+// })
+a= document.links
+console.log(a)
